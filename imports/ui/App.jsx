0ade3031814f7378ab42a84929cd28a361f65539
Alan Gustavo Valdez Cascajares <Alan Gustavo Valdez Cascajares>
2017-02-17 11:30:21 -0600

    Versión actualizada de tutorial, anterior era una versión vieja.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,7 @@
  * Created by agval on 2/7/2017.
  */
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js'
@@ -10,8 +11,36 @@ import Task from './Task.jsx';
 
 //App component - Represents the whole app
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: false,
+        };
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+        Tasks.insert({text, createdAt: new Date(),});
+
+        ReactDOM.findDOMNode(this.refs.textInput).value = "";
+    }
+
+    toggleHideCompleted() {
+        this.setState({hideCompleted: !this.state.hideCompleted,});
+    }
+
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;
+
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+
+        return filteredTasks.map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -20,7 +49,15 @@ class App extends Component {
         return (
             <div className="container">
                 <header>
-                    <h1>To do List</h1>
+                    <h1>To do List ({this.props.incompleteCount})</h1>
+                    <label className="hide-completed">
+                        <input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted.bind(this)}/>
+                        Hide completed tasks
+                    </label>
+
+                    <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+                        <input type="text" ref="textInput" placeholder="Type to add new tasks"/>
+                    </form>
                 </header>
 
                 <ul>
@@ -33,10 +70,12 @@ class App extends Component {
 
 App.propTypes = {
     tasks: PropTypes.array.isRequired,
+    incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
     return {
-        tasks: Tasks.find({}).fetch(),
+        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+        incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
     };
 }, App);
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,10 +1,31 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Tasks } from '../api/tasks.js';
+
 //Task component - represents a single to do item
 export default class Task extends Component {
+    toggleChecked() {
+        Tasks.update(this.props.task._id, {
+           $set: { checked: !this.props.task.checked }
+        });
+    }
+
+    deleteTask() {
+        Tasks.remove(this.props.task._id);
+    }
+
     render() {
+        const taskClassName = this.props.task.checked ? 'checked' : '';
+
         return (
-            <li>{this.props.task.text}</li>
+            <li className={taskClassName}>
+                <button className="delete" onClick={this.deleteTask.bind(this)}>
+                    &times;
+                </button>
+
+                <input type="checkbox" checked={this.props.task.checked} onClick={this.toggleChecked.bind(this)}/>
+                <span className="text">{this.props.task.text}</span>
+            </li>
         );
     }
 }
