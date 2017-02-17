561075f37ba1512b6becfebb091e3f5e7572e78f
Alan Gustavo Valdez Cascajares <Alan Gustavo Valdez Cascajares>
2017-02-17 09:05:30 -0600

    Commit con todos los tutoriales realizados para entender el funcionamiento de MeteorJS con React

---
+++ imports/api/tasks.js
@@ -0,0 +1,3 @@
+import { Collection } from 'meteor/mongo';
+
+export const Tasks = new Mongo.Collection('tasks');
---
+++ imports/ui/App.jsx
@@ -0,0 +1,42 @@
+/**
+ * Created by agval on 2/7/2017.
+ */
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js'
+
+import Task from './Task.jsx';
+
+//App component - Represents the whole app
+class App extends Component {
+    renderTasks() {
+        return this.props.tasks.map((task) => (
+            <Task key={task._id} task={task} />
+        ));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>To do List</h1>
+                </header>
+
+                <ul>
+                    {this.renderTasks()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+    tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+    return {
+        tasks: Tasks.find({}).fetch(),
+    };
+}, App);
